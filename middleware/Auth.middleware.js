f7fb57af42d8b1fa777ac0b6af8805198f8717cf
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-06-12 23:18:16 +0200

    install new packages, remove a console.log from aut.middleware.js

--- middleware/Auth.middleware.js
+++ middleware/Auth.middleware.js
@@ -8,8 +8,6 @@ const getTokenFromHeader = (req, res, next) => {
     ) {
         const token = req.headers.authorization.split(" ")[1];
 
-        console.log("Token:", token);
-
         jwt.verify(token, SECRET, (err, decoded) => {
             if (err) {
                 return res.status(401).json({ error: "Invalid token" });
